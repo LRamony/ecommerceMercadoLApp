b326736f71dde2cbcad22cb4376d7a616261fa39
Lucas Ramony <lramony>
2020-11-28 10:38:37 -0300

    layout adicionado

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Criando CATEGORIAS...."
+categories = ["Animais e Acessorios",
+"Esporte",
+"Para a sua casa",
+"Eletronicos e Celulares",
+"Musica e hobbies",
+"Bebes e crianças",
+"Moda e Beleza",
+"Veiculos e Barcos",
+"Imoveis",
+"Empregos e Negocios"]
+
+categories.each do |categoriess|
+    Category.find_or_create_by!(description: categoriess )
+end    
+puts "CATEGORIAS casdastradas com Sucesso!!!"
