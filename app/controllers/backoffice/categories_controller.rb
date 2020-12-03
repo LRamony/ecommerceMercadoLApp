f1ea42bad1239acfd96acc1f0938e2733ebd0004
Lucas Ramony <lramony>
2020-12-03 10:30:17 -0300

    criando buttons de rota manual com resource

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -3,4 +3,17 @@ class Backoffice::CategoriesController < BackofficeController
   def index
     @Categorias = Category.all
   end
+
+  def new
+  end
+
+  def create 
+  end 
+  
+  def edit 
+  end
+  
+  def update 
+  end  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  
-  namespace :backoffice do
-    get 'categories/index'
-  end
+  root 'site/home#index'
+
   get 'admin', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
+    #only: [:edit, :update, :show, :destory] <- so quero mostrar
+    resources :categories, except: [:show] #<- so n quero mostrar
+    get 'categories/index'
     get 'dashboard', to: 'dashboard#index'
   end
+
   namespace :site do
     get 'home', to: 'home#index'
   end
+
   devise_for :admins
   devise_for :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'site/home#index'
 end
